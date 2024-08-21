bbc998412ef17ac3dad48ce46e7adc2f8d1e08ed
klivesSTACK <larrybriones10@gmail.com>
2024-08-21 16:17:46 +0800

    First Commit

---
+++ controllers/workout.js
@@ -0,0 +1,108 @@
+const User = require("../models/User");
+const auth = require('../auth');
+const WorkOut = require("../models/WorkOut");
+
+// add workout endoint
+module.exports.addWorkout =  (req, res) => {
+
+    // instantite new workout
+    let newWorkout = new WorkOut({
+        userId: req.user.id,
+        name: req.body.name,
+        duration: req.body.duration
+    });
+
+    // scan workout database
+    WorkOut.findOne({ name: req.body.name, userId: req.user.id }).then(workoutExist => {
+        // if workout name already exist in database
+        if(workoutExist){
+            return res.status(409).send({ message: 'Workout already listed'})
+        // if no workout name exist in database. create and save the newly created workout
+        }else {
+            return newWorkout.save().then(workout => {
+                res.status(201).send({workout})
+            }).catch(err => errorHandler(err, req, res));
+        }
+    })
+
+} 
+
+// get workout endpoint
+module.exports.getMyWorkouts = (req, res) => {
+
+    //scan and fine all workout in database
+    WorkOut.find({ userId: req.user.id }).then(result => {
+        //return response from database
+        return res.status(200).send({ workouts: result})
+    }).catch(err => errorHandler(err, req, res));
+}
+
+// updated workout endpoint
+module.exports.updateWorkout = (req, res ) => {
+
+    //deconstructing
+    const { name, duration } = req.body;
+
+    // initialize 
+    const id = req.params.workoutId;
+
+
+    let updatedWorkout = {
+        name: name,
+        duration: duration
+    }
+
+    // scan the database and update
+    WorkOut.findByIdAndUpdate( id, updatedWorkout, { new: true} )
+    .then(result => {
+        // if successfull
+        if(result){
+            return res.status(200).send({
+                message: 'Workout updated successfully',
+                updatedWorkout: result
+            })
+        // if not
+        }else {
+            res.status(404).send({ message: 'Workout not found'})
+        }
+    }).catch(err => errorHandler(err, req, res));
+
+}
+
+// delete workout enpoint
+module.exports.deleteWorkout = (req, res) => {
+
+    // initialize 
+    const id = req.params.workoutId;
+
+    // Scan and delete workout in database
+    WorkOut.findByIdAndDelete(id, {new: true}).then( deleted =>{
+
+        // if success
+        if(deleted){
+            return res.status(200).send({ message: 'Workout deleted sucessfully'})
+        //if not
+        }else {
+            return res.status(400).send({ message: 'No workout found'})
+        }
+    })
+
+}
+
+// workout status endpoint
+module.exports.completeWorkoutStatus = ( req, res ) => {
+
+    const id = req.params.workoutId;
+
+    WorkOut.findByIdAndUpdate( id, { status: 'completed' }, { new: true}).then( completed => {
+        if(completed){
+            return res.status(200).send({ 
+                message: 'Workout status updated successfully',
+                updatedWorkout: completed
+            })
+        }else {
+            return res.status(400).send({ message: 'Workout status updating failed'});
+        }
+    })
+
+}
---
+++ routes/workout.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const workoutController = require("../controllers/workout");
+const { verify } = require("../auth");
+
+//Routing component
+const router = express.Router();
+
+router.post('/addWorkout', verify, workoutController.addWorkout);
+router.get('/getMyWorkouts', verify, workoutController.getMyWorkouts );
+router.patch('/updateWorkout/:workoutId', verify, workoutController.updateWorkout);
+router.delete('/deleteWorkout/:workoutId', verify, workoutController.deleteWorkout);
+router.patch('/completeWorkoutStatus/:workoutId', verify, workoutController.completeWorkoutStatus);
+
+module.exports = router;
